7dd06430edb63680d74bc2856ef07a6acc5b635b
santiago-diaz-repo <santiago.diaz5689@gmail.com>
2021-10-25 11:47:58 -0500

    add travis

--- calculator_test.go
+++ calculator_test.go
@@ -1,14 +1,17 @@
 package travis_ci_go_practice
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
 
 func Test_Sum(t *testing.T){
 	want := 11
 	subject := Calculator{}
 	got := subject.Sum(5,6)
-	if got != want {
-		t.Errorf("wanted: %d, got: %d",want,got)
-	}
+
+	assert.Equal(t, want, got, "they should not be equal")
 }
 
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module travis-ci-go-practice
 
 go 1.16
+
+require github.com/stretchr/testify v1.7.0
